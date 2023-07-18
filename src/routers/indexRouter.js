bffa0bd9ce785b4ec81cd593d8587ccdc3ec7c0c
Aram Chernousov <aram.chernousov@gmail.com>
2023-07-18 21:52:25 +0300

    half footer

--- src/routers/indexRouter.js
+++ src/routers/indexRouter.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 const renderTemplate = require('../lib/renderTemplate');
 const Home = require('../views/Home');
 
-router.get('/', (req, res) => {
-  renderTemplate(Home, {}, res);
+const { Category } = require('../../db/models');
+
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({ raw: true });
+    renderTemplate(Home, { categories }, res);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;
