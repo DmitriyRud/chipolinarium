c5e7c9516c6670dbce4c1dcb1f8d6f225d24f820
Aram Chernousov <aram.chernousov@gmail.com>
2023-07-18 19:26:43 +0300

    seeders fix

---
+++ db/seeders/20230718162350-CreateUser.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const hash = await bcrypt.hash('123', 10);
+    await queryInterface.bulkInsert('Users', [
+      {
+        email: 'admin@admin.ru',
+        password: hash,
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
