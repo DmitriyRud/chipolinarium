259f9a75d89f344b9c94bf3fa1eda6171b58c29f
Aram Chernousov <aram.chernousov@gmail.com>
2023-07-19 15:49:19 +0300

    feedbacks

--- src/routers/feedbackRouter.js
+++ src/routers/feedbackRouter.js
@@ -2,13 +2,34 @@ const feedBackRouter = require('express').Router();
 
 const renderTemplate = require('../lib/renderTemplate');
 
-const Feedback = require('../views/Feedback');
+const Feedbacks = require('../views/Feedbacks');
 
-const { Category } = require('../../db/models');
+const { Category, Feedback } = require('../../db/models');
 
 feedBackRouter.get('/', async (req, res) => {
   const categories = await Category.findAll({ raw: true });
-  renderTemplate(Feedback, { categories }, res);
+  renderTemplate(Feedbacks, { categories }, res);
+});
+
+feedBackRouter.post('/', async (req, res) => {
+  const { email, name, body } = req.body;
+  try {
+    const newFeedback = await Feedback.create({
+      email,
+      name,
+      body,
+      approved: false,
+    });
+    if (newFeedback) {
+      res.json({
+        msg: 'Ваш отзыв успешно создан! После модерации он отобразится на сайте.',
+      });
+    } else {
+      res.json({ error: 'Что-то пошло не так!' });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = feedBackRouter;
