9889091cd560dd4acf784f19645b38913c8f6dbb
Aram Chernousov <aram.chernousov@gmail.com>
2023-07-19 22:10:30 +0300

    post delivery

---
+++ public/js/delivery.js
@@ -0,0 +1,81 @@
+const deliveryTable = document.getElementById('delivery_table');
+const paragraph = document.querySelector('.alert_table');
+const newDelivery = document.getElementById('newDelivery');
+const closeFormBtn = document.querySelector('.closeFormBtn');
+const deliveryForm = document.getElementById('deliveryForm');
+
+deliveryTable.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (e.target.tagName === 'BUTTON') {
+    try {
+      const id = e.target.dataset.deleteDelivery;
+      const response = await fetch(`/delivery/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id }),
+      });
+      const result = await response.json();
+      if (result.msg) {
+        const tr = document.getElementById(`tr-${id}`);
+        tr.remove();
+      } else if (result.error) {
+        paragraph.innerText = result.error;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
+
+newDelivery.addEventListener('click', (event) => {
+  deliveryForm.style.display = 'block';
+  newDelivery.style.display = 'none';
+});
+
+closeFormBtn.addEventListener('click', (event) => {
+  deliveryForm.style.display = 'none';
+  newDelivery.style.display = 'block';
+});
+
+deliveryForm.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const formData = new FormData(deliveryForm);
+  const inputs = Object.fromEntries(formData);
+  if (
+    e.target.classList.contains('addFormBtn') &&
+    e.target.tagName === 'BUTTON'
+  ) {
+    console.log('=============>');
+    try {
+      const response = await fetch('/delivery', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(inputs),
+      });
+      const result = await response.json();
+      if (result.msg) {
+        paragraph.innerText = result.msg;
+        deliveryForm.style.display = 'none';
+        //   const tr = document.createElement('tr');
+        //   const tdValue = Object.values(result);
+        //   tdValue.shift();
+        //   for (let i = 0; i < 2; i += 1) {
+        //     const td = document.createElement('td');
+        //     td.innerText = tdValue[i];
+        //     tr.appendChild(td);
+        //   }
+        //   const tBody = document.querySelector('tbody');
+        //   tBody.appendChild(tr);
+        window.location.reload();
+      } else if (result.error) {
+        paragraph.innerText = result.error;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
--- src/routers/deliveryRouter.js
+++ src/routers/deliveryRouter.js
@@ -2,10 +2,46 @@ const deliveryRouter = require('express').Router();
 
 const renderTemplate = require('../lib/renderTemplate');
 
-const Delivery = require('../views/Delivery');
+const DeliveryPage = require('../views/DeliveryPage');
 
-deliveryRouter.get('/', (req, res) => {
-  renderTemplate(Delivery, {}, res);
+const { Category, Delivery } = require('../../db/models');
+
+deliveryRouter.get('/', async (req, res) => {
+  const categories = await Category.findAll({ raw: true });
+  const deliveries = await Delivery.findAll({ raw: true });
+  renderTemplate(DeliveryPage, { categories, deliveries }, res);
+});
+
+deliveryRouter.post('/', async (req, res) => {
+  const { order_price: orderPrice, delivery_price: deliveryPrice } = req.body;
+  try {
+    const newDelivery = await Delivery.create({
+      order_price: orderPrice,
+      delivery_price: deliveryPrice,
+    });
+    if (newDelivery) {
+      res.json({
+        msg: 'Тариф доставки успешно добавлен!',
+        orderPrice: newDelivery.order_price,
+        deliveryPrice: newDelivery.delivery_price,
+      });
+    } else {
+      res.json({ error: 'Что-то пошло не так!' });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+deliveryRouter.delete('/:id', async (req, res) => {
+  const { id } = req.body;
+  try {
+    await Delivery.destroy({ where: { id } });
+    res.json({ msg: 'Тариф доставки удален' });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: 'Тариф доставки не удален' });
+  }
 });
 
 module.exports = deliveryRouter;
