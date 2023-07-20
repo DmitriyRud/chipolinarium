acd83940cf9a0c03c44a0b82a1e39375ddf77fd3
Elizaveta Nikolina <nikolina.elizavet@gmail.com>
2023-07-20 17:08:14 +0300

    catalog

--- src/views/Catalog.jsx
+++ src/views/Catalog.jsx
@@ -12,39 +12,104 @@ module.exports = function Catalog({ categories, email }) {
         </a>
       </div>
       {email ? (
-        <div className="container">
+        <div id={categories.id} className="container category-container">
           <a href="/accountPanel"> Добавить новую категорию</a>
           <div className="containerCategory">
             {categories.length ? (
               categories.map((category) => (
                 <div className="card" key={`${category.id}item`}>
-                  <div>
+                  <div
+                    id={`card-hidden-${category.id}`}
+                    className="card-hidden"
+                  >
                     <img
+                      id={`card-img-top-${category.id}`}
                       src={category.image}
                       className="card-img-top"
                       alt="..."
                     />
                     <div className="card-body">
-                      <h5 className="card-title">{category.title}</h5>
-                      <p className="card-text">{category.description}</p>
+                      <h5
+                        id={`card-title-${category.id}`}
+                        className="card-title"
+                      >
+                        {category.title}
+                      </h5>
+                      <p id={`card-text-${category.id}`} className="card-text">
+                        {category.description}
+                      </p>
                     </div>
                     <div className="card-btn">
                       <a className="btn" href={`/catalog/${category.id}`}>
                         {' '}
                         подробнее
                       </a>
-                      <a className="btn editBtn" href="#" id={category.id}>
+                      <button
+                        type="button"
+                        className="btn editBtn"
+                        data-edit-category={category.id}
+                      >
                         изменить
-                      </a>
-                      <a
+                      </button>
+                      <button
+                        type="button"
                         className="btn deleteBtn"
-                        href="#"
-                        id={category.id}
-                        data-delid={category.id}
+                        data-delete-item={category.id}
                       >
                         удалить
-                      </a>
+                      </button>
                     </div>
+                  </div>
+                  <div
+                    className="hide_container_form"
+                    style={{ display: 'none' }}
+                    id={`hide-form-${category.id}`}
+                  >
+                    <p style={{ color: 'red' }} className="alert_items" />
+                    <form
+                      id={`editCategory-${category.id}`}
+                      encType="multipart/form-data"
+                      name="newCategory"
+                    >
+                      {/* <select name="categoryName" id="">
+                        {categories.length ? (
+                          categories.map((category) => (
+                            <option selected value={category.title}>
+                              {category.title}
+                            </option>
+                          ))
+                        ) : (
+                          <span> </span>
+                        )}
+                      </select> */}
+
+                      <label htmlFor="pic_input">*Фото:</label>
+                      <input id="pic_input" type="file" name="photo" />
+
+                      <label htmlFor="title">Название</label>
+                      <input
+                        id="title"
+                        type="text"
+                        name="title"
+                        value={category.title}
+                      />
+
+                      <label htmlFor="description">Описание товара</label>
+                      <input
+                        id="description"
+                        type="text"
+                        name="description"
+                        value={category.description}
+                      />
+
+                      <button
+                        type="button"
+                        className="btn btn-primary sendCategory"
+                        data-send-edit-category={category.id}
+                      >
+                        Отправить
+                      </button>
+                    </form>
                   </div>
                 </div>
               ))
