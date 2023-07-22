b30dd2d33505989f09c23bffc8806ff7f7181d08
Aram Chernousov <aram.chernousov@gmail.com>
2023-07-22 19:19:55 +0300

    modal style & validation

--- public/js/modalHeader.js
+++ public/js/modalHeader.js
@@ -11,6 +11,8 @@ const modalList = document.querySelector('.modal_list');
 
 const closeModalWindow = document.querySelector('.closeModalWindow');
 
+const priceFormAlertModal = document.querySelector('.price_form_alert_modal');
+
 buttonPrice.addEventListener('click', (event) => {
   dialog.showModal();
 });
@@ -34,7 +36,51 @@ dialogForm.addEventListener('submit', async (event) => {
 
   const data = new FormData(dialogForm);
   const inputs = Object.fromEntries(data);
-  dialog.close();
+
+  const inputValidationName = inputs.name.trim();
+  const inputValidationEmail = inputs.email.trim();
+  const inputValidationPhone = inputs.phone.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+  const namePattern = /^[A-Za-zА-Яа-яЁё\s]*$/g;
+  const phonePattern = /^\+?[0-9()-\s]+$/g;
+
+  if (!inputValidationName || !inputValidationEmail || !inputValidationPhone) {
+    priceFormAlertModal.innerText = 'Поля не могут быть пустыми!';
+    priceFormAlertModal.style.color = 'red';
+  } else if (inputs.name.match(namePattern) === null) {
+    priceFormAlertModal.innerText = 'Некорректное имя';
+    priceFormAlertModal.style.color = 'red';
+  } else if (inputs.phone.match(phonePattern) === null) {
+    priceFormAlertModal.innerText = 'Некорректный номер телефона';
+    priceFormAlertModal.style.color = 'red';
+  } else if (inputs.email.match(emailPattern) === null) {
+    priceFormAlertModal.innerText = 'Некорректный email';
+    priceFormAlertModal.style.color = 'red';
+  } else {
+    try {
+      const response = await fetch('/modalpice', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(inputs),
+      });
+
+      priceFormAlertModal.innerText =
+        'Ваше письмо отправлено! Менеджеры свяжутся с Вами в ближайшее время.';
+      priceFormAlertModal.style.color = '#338023';
+
+      inputsOnHome.forEach((el) => (el.value = ''));
+
+      setTimeout(() => {
+        dialog.close();
+        priceFormAlertModal.innerText = '';
+      }, 5000);
+    } catch (error) {
+      console.log('что-то пошло не так', error);
+    }
+  }
+
   //   modalPriceBtn.addEventListener('click', (event) => {
   //     Email.send({
   //       Host: 'smtp.elasticemail.com',
@@ -50,16 +96,4 @@ dialogForm.addEventListener('submit', async (event) => {
   //       Для уточнения данных пользователь оставил номер телефона: ${inputs.phone}`,
   //     }).then((message) => dialog.close());
   //   });
-
-  try {
-    const response = await fetch('/modalpice', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(inputs),
-    });
-  } catch (error) {
-    console.log('что-то пошло не так', error);
-  }
 });
--- public/js/requestPriceHome.js
+++ public/js/requestPriceHome.js
@@ -1,5 +1,5 @@
 const formRequestPriceHome = document.querySelector('.formRequestPriceHome');
-const priceFormDescription = document.querySelector('.formOnHome');
+const priceFormAlertHome = document.querySelector('.price_form_alert_home');
 const inputsOnHome = document.querySelectorAll('input');
 
 formRequestPriceHome.addEventListener('submit', async (event) => {
@@ -8,19 +8,46 @@ formRequestPriceHome.addEventListener('submit', async (event) => {
   const data = new FormData(formRequestPriceHome);
   const inputs = Object.fromEntries(data);
 
-  try {
-    const response = await fetch('/modalpice', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(inputs),
-    });
+  const inputValidationName = inputs.name.trim();
+  const inputValidationEmail = inputs.email.trim();
+  const inputValidationPhone = inputs.phone.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+  const namePattern = /^[A-Za-zА-Яа-яЁё\s]*$/g;
+  const phonePattern = /^\+?[0-9()-\s]+$/g;
 
-    priceFormDescription.innerText = 'Ваше письмо отправлено! Менеджеры свяжутся с Вами в ближайшее время.';
+  if (!inputValidationName || !inputValidationEmail || !inputValidationPhone) {
+    priceFormAlertHome.innerText = 'Поля не могут быть пустыми!';
+    priceFormAlertHome.style.color = 'red';
+  } else if (inputs.name.match(namePattern) === null) {
+    priceFormAlertHome.innerText = 'Некорректное имя';
+    priceFormAlertHome.style.color = 'red';
+  } else if (inputs.phone.match(phonePattern) === null) {
+    priceFormAlertHome.innerText = 'Некорректный номер телефона';
+    priceFormAlertHome.style.color = 'red';
+  } else if (inputs.email.match(emailPattern) === null) {
+    priceFormAlertHome.innerText = 'Некорректный email';
+    priceFormAlertHome.style.color = 'red';
+  } else {
+    try {
+      const response = await fetch('/modalpice', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(inputs),
+      });
 
-    inputsOnHome.forEach((el) => (el.value = ''));
-  } catch (error) {
-    console.log('что-то пошло не так', error);
+      priceFormAlertHome.innerText =
+        'Ваше письмо отправлено! Менеджеры свяжутся с Вами в ближайшее время.';
+      priceFormAlertHome.style.color = '#338023';
+
+      inputsOnHome.forEach((el) => (el.value = ''));
+
+      setTimeout(() => {
+        priceFormAlertHome.innerText = '';
+      }, 15000);
+    } catch (error) {
+      console.log('что-то пошло не так', error);
+    }
   }
 });
